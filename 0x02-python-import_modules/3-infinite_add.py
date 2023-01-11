13372f89c05ec8ef00c8768032a76dcc891d4602
Kenechukwu Okafor <okaforkene2@gmail.com>
2023-01-11 04:01:53 -0800

    changes to infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    sum = 0
-    if len(argv) > 1:
-        for i in range(1, len(argv)):
-            sum += int(argv[i])
-    print(sum)
+    import sys
+num = len(sys.argv) - 1
+total = 0
+for i in range(num + 0):
+    total = total + int(sys.argv[i + 1])
+print (total)
